0e4b38ffafdbc6ceb318ec0c4b888125e32b227e
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2017-05-17 16:32:12 +0800

    Add console support
    Change-Id: I0b845d8d2ac9bee30676b1d6e01609c71ffeae48

--- mogan_ui/content/project/servers/tabs.py
+++ mogan_ui/content/project/servers/tabs.py
@@ -15,7 +15,10 @@
 
 from django.utils.translation import ugettext_lazy as _
 
+from horizon import exceptions
 from horizon import tabs
+
+from mogan_ui.api import mogan
 
 
 class OverviewTab(tabs.Tab):
@@ -29,7 +32,25 @@ class OverviewTab(tabs.Tab):
                 "is_superuser": request.user.is_superuser}
 
 
+class ConsoleTab(tabs.Tab):
+    name = _("Console")
+    slug = "console"
+    template_name = "project/servers/_detail_console.html"
+    preload = False
+
+    def get_context_data(self, request):
+        server = self.tab_group.kwargs['server']
+        console_url = None
+        try:
+            console = mogan.console_get(request, server.uuid)
+            console_url = console.console['url']
+        except Exception:
+            exceptions.handle(request, ignore=True, force_log=True)
+
+        return {'console_url': console_url, 'server_id': server.uuid}
+
+
 class ServerDetailTabs(tabs.DetailTabsGroup):
     slug = "server_details"
-    tabs = (OverviewTab,)
+    tabs = (OverviewTab, ConsoleTab)
     sticky = True
